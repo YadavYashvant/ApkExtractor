f731371363a7ca9181b245f5e45a1560022ca4d8
YadavYashvant <yashvantyadav855@gmail.com>
2025-07-12 22:04:49 +0530

    changing UI

---
+++ app/src/main/java/com/yashvant/zyptra/util/Config.kt
@@ -0,0 +1,6 @@
+package com.yashvant.zyptra.util
+
+object Config {
+    const val OAUTH_CLIENT_ID = "64130373589-rbeobu1ggl9oobv4v0bhvn00rvlmqfvd.apps.googleusercontent.com"
+    const val DRIVE_SCOPE = "https://www.googleapis.com/auth/drive.file"
+}
